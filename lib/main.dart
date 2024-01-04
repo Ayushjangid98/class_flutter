def69ff4800232cc1d6523149dc749222bcf8a32
Dilip-lohar <68553416+Dilip-lohar@users.noreply.github.com>
2024-01-04 14:20:34 +0530

    class model

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
+import 'list_class.dart';
+
 void main() {
+  listFunction();
   runApp(const MyApp());
 }
 
@@ -31,70 +34,8 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  List<int> ids = [1, 2, 4, 56];
 
-  List<Map<String, dynamic>> user = <Map<String, dynamic>>[
-    {
-      "id": 1,
-      "names": "Bartholomew",
-      "email": "brobarts0@toplist.cz",
-      "phone_no": "481-306-3783"
-    },
-    {
-      "id": 2,
-      "name": "Gunar",
-      "email": "gliell1@adobe.com",
-      "phone_no": "455-735-6609"
-    },
-    {
-      "id": 3,
-      "names": "Annabell",
-      "email": "awannell2@weather.com",
-      "phone_no": "210-604-7984"
-    },
-    {
-      "id": 4,
-      "name": "Vevay",
-      "email": "vamorine3@t.co",
-      "phone_no": "345-736-0286"
-    },
-    {
-      "id": 5,
-      "name": "Lucita",
-      "email": "ldabbotdoyle4@amazon.de",
-      "phone_no": "741-926-7899"
-    },
-    {
-      "id": 6,
-      "name": "Linea",
-      "email": "llydon5@mysql.com",
-      "phone_no": "902-190-4865"
-    },
-    {
-      "id": 7,
-      "name": "Katherine",
-      "email": "kpetican6@clickbank.net",
-      "phone_no": "515-408-8880"
-    },
-    {
-      "id": 8,
-      "name": "Wallis",
-      "email": "wsaltmarshe7@behance.net",
-      "phone_no": "587-449-5806"
-    },
-    {
-      "id": 9,
-      "name": "Barbey",
-      "email": "bscanlan8@newsvine.com",
-      "phone_no": "578-574-8274"
-    },
-    {
-      "id": 10,
-      "name": "Moshe",
-      "email": "mbloxland9@live.com",
-      "phone_no": "663-526-1043"
-    }
-  ];
+
   List<User> userModelList = <User>[
     // User(id: 1, email: "xyz@gmail.com", name: "xyz", phone: "2345678987654")
     User(name: "name", phone: "phone", id: 1)
@@ -108,8 +49,66 @@ class _TestState extends State<Test> {
     }),
   ];
 
+
+
+
+
+
+
+
+  List<int> ids = [1, 2, 4, 56];
+
+
+  List<User> userTable = <User>[];
+  List<Map<String, dynamic>> user = <Map<String, dynamic>>[
+    {
+      "id": 1,
+      "name": "Bartholomew",
+      "email": "brobarts0@toplist.cz",
+      "phone_no": "481-306-3783"
+    },
+    {
+      "id": 2,
+      "name": "Gunar",
+      "email": "gliell1@adobe.com",
+      "phone_no": "455-735-6609"
+    },
+    {
+      "id": 3,
+      "name": "Annabell",
+      "email": "awannell2@weather.com",
+      "phone_no": "210-604-7984"
+    },
+  ];
+  void addMonitor(){
+    for (int i = 0; i < user.length; i++) {
+      userTable.add(
+        User(name: user[i]["name"], phone: user[i]["phone_no"],id: user[i]["id"],email: user[i]["email"]),
+      );
+    }
+
+    // for(User user in userTable){
+    //   print(user.name);
+    // }
+
+    for (var user in userTable) {
+      print(user.name);
+    }
+  }
+
   @override
   void initState() {
+    /// class type list
+    // print(userTable[1].phone);
+
+    /// Map<String,dynamic> type list
+    print(user[0]["name"]);
+    addMonitor();
+    print(userTable[1].name);
+
+    print(userTable.first.name);
+
+
     // print(ids.first);
     // print(userModelList.first.name);
     // print(user.first["name"]);
@@ -176,7 +175,8 @@ class _TestState extends State<Test> {
         "phone_no": "663-526-1043"
       }
     ].map((x) => UserModel.fromJson(x)));
-    print(userQuickModelList.first.phoneNo);
+    // print(userQuickModelList.first.phoneNo);
+
 
 
     super.initState();
@@ -189,10 +189,10 @@ class _TestState extends State<Test> {
 }
 
 class User {
-  int? id;
+  int ? id;
   String name;
   String phone;
-  String? email;
+  String ? email;
   User({
     this.id,
     this.email,
@@ -227,3 +227,5 @@ class UserModel {
         "phone_no": phoneNo,
       };
 }
+
+
